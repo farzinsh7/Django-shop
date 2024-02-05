e30d6e1a4d640f9fdbe21b54ae63626ce1391855
farzinsh7 <farzinnater@gmail.com>
2024-02-05 11:18:28 +0330

    Finish customize Products Admin-panel Part1

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Product, Tags, Category, Gallery
+from django.forms import Textarea, TextInput
+from django.db import models
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    list_filter = (['status'])
+    search_fields = ('title', 'slug')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    list_filter = (['status'])
+    search_fields = ('title', 'slug')
+
+
+admin.site.register(Tags, TagsAdmin)
+
+class GalleriesAdmin(admin.TabularInline):
+    model = Gallery
+    extra = 1
+    readonly_fields = ('image_preview',)
+    formfield_overrides = {
+            models.CharField: {'widget': TextInput(attrs={'size':'30'})},
+        }
+
+    def image_preview(self, obj):
+        # ex. the name of column is "image"
+        if obj.image:
+            return mark_safe('<img src="{0}" width="100" height="100" style="object-fit:contain" />'.format(obj.image.url))
+        else:
+            return '(No image)'
+
+    image_preview.short_description = 'Preview'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [GalleriesAdmin]
+    prepopulated_fields = {"slug": ["title"]}
+    list_display = ('thumbnail_tag', 'title')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        # ex. the name of column is "image"
+        if obj.image:
+            return mark_safe('<img src="{0}" width="150" height="150" style="object-fit:contain" />'.format(obj.image.url))
+        else:
+            return '(No image)'
+    
+    image_preview.short_description = 'Preview'
--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.html import format_html
 
 # Create your models here.
 class Category(models.Model):
@@ -27,14 +28,14 @@ class Product(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     description = RichTextUploadingField(null=True)
     short_description = RichTextUploadingField(null=True)
-    image = models.ImageField(upload_to='shop/image', null=True, blank=True)
     regular_price = models.IntegerField()
     sale_price = models.IntegerField(null=True, blank=True)
-    sku = models.IntegerField(unique=True)
-    dimenstions = models.IntegerField(null=True, blank=True)
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    dimenstions = models.CharField(max_length=200, null=True, blank=True)
     stock_quantity = models.IntegerField(default=0)
     category = models.ManyToManyField(Category, related_name='category')
     tag = models.ManyToManyField(Tags, related_name='tag')
+    image = models.ImageField(upload_to='shop/image', null=True, blank=True)
     # tax_class = pass
     # shiping_class = pass
     # related_product = pass
@@ -43,6 +44,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def thumbnail_tag(self):
+        return format_html(
+            "<img width=100 height=75 style='border-radius: 5px;' src='{}'>".format(self.image.url))
+
+    thumbnail_tag.short_description = "Thumbnail"
     
 
 class Gallery(models.Model):
