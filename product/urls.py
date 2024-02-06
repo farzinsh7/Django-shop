321e6f2b1cf8ecec400ec3c5f7ef2ec032379822
farzinsh7 <farzinnater@gmail.com>
2024-02-06 15:12:45 +0330

    Finish setting for product list-view

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import ProductListView, ProductDetailView
 
 
 app_name = "product"
 urlpatterns = [
-    # path('shop/', ArticlesListView.as_view(), name="list_view"),
-    # path('shop/<slug:slug>', ArticleDetailView.as_view(), name='detail_view'),
+    path('shop/', ProductListView.as_view(), name="list_view"),
+    path('shop/<slug:slug>', ProductDetailView.as_view(), name='detail_view'),
 ]
