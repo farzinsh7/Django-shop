b8148840894e3bbbe8eaa9fcd2d8b3a9ce78ff46
Farzin Shams <farzinnater@gmail.com>
2024-12-12 12:13:00 +0330

    Start Website App

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My_APPS
+    'website'
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -17,12 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
 ]
 
 
 if settings.SHOW_DEBUGGER_TOOLBAR:
     urlpatterns += [path('__debug__/', include('debug_toolbar.urls')),
                     ]
+    
+
+if settings.DEBUG:
+    # add root static files
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # add media static files
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
