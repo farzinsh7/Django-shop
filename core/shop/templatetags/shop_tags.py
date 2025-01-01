289c635d890a51f3a4f8675c27e59073f31518c4
Farzin Shams <farzinnater@gmail.com>
2025-01-01 18:13:19 +0330

    Added similar-products by category in product detail template

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -9,3 +9,11 @@ def show_latest_products():
     latest_products = models.Product.objects.prefetch_related("category").filter(
         status=models.StatusType.publish.value).order_by("-created_at")[:8]
     return {"latest_products": latest_products}
+
+
+@register.inclusion_tag("includes/similar-products.html")
+def show_similar_products(product):
+    product_categories = product.category.all()
+    similar_products = models.Product.objects.prefetch_related("category").filter(
+        status=models.StatusType.publish.value, category__in=product_categories).exclude(id=product.id).distinct().order_by("-created_at")[:4]
+    return {"similar_products": similar_products}
