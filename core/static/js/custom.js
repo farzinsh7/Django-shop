ac284934ce2f720b8da0cbb53c8c94758400004f
Farzin Shams <farzinnater@gmail.com>
2025-01-02 15:00:12 +0330

    Doing some Changes

--- core/static/js/custom.js
+++ core/static/js/custom.js
@@ -1,26 +1,3 @@
-$(document).ready(function(){
-    let current_url_params = new URLSearchParams(window.location.search)
-    $("#page-size-filter").val(current_url_params.get("page_size") || "")
-    $("#search-query-filter").val(current_url_params.get("q") || "")
-    $("#order-by-filter").val(current_url_params.get("order_by") || "")
-    $("#category-id-filter").val(current_url_params.get("category_id") || "")
-    $("#min-price-filter").val(current_url_params.get("min_price") || "")
-    $("#max-price-filter").val(current_url_params.get("max_price") || "")
-})
-$("#page-size-filter").change(function(){
-    let current_url_params = new URLSearchParams(window.location.search)
-    var selectedOption = $(this).val();
-    current_url_params.set("page_size", selectedOption)
-    let new_url = window.location.pathname + "?" + current_url_params.toString()
-    window.location.href = new_url
-})
-$("#order-by-filter").change(function(){
-    let current_url_params = new URLSearchParams(window.location.search)
-    var selectedOption = $(this).val();
-    current_url_params.set("order_by", selectedOption)
-    let new_url = window.location.pathname + "?" + current_url_params.toString()
-    window.location.href = new_url
-})
 function changePage(page_number){
     let current_url_params = new URLSearchParams(window.location.search)
     current_url_params.set("page", page_number)
