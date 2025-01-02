4d75cbf93e7fbeb61340c9256e3971e15c2102ea
Farzin Shams <farzinnater@gmail.com>
2025-01-02 14:41:35 +0330

    Added cart-summary page

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -6,4 +6,6 @@ app_name = "cart"
 urlpatterns = [
     path('session/add-product/', views.SessionAddProduct.as_view(),
          name='session-add-product'),
+    path('session/cart/summary/', views.SessionCartSummary.as_view(),
+         name='session-cart-summary'),
 ]
