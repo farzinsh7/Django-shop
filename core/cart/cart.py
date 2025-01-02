dc33ecef60e47266ae2897cbab08b99f9c801390
Farzin Shams <farzinnater@gmail.com>
2025-01-02 13:16:59 +0330

    Created a View for Cart

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -1,10 +1,37 @@
 class CartSession:
     def __init__(self, session):
         self.session = session
-        self.cart = self.session.get("cart",
-                                     {
-                                         "items": [],
-                                         "total_price": 0,
-                                         "total_items": 0
-                                     })
-        self.session["cart"] = self.cart
+        self._cart = self.session.setdefault("cart", {
+            "items": [],
+        })
+
+    def add_product(self, product_id):
+        for item in self._cart["items"]:
+            if product_id == item["product_id"]:
+                item["quantity"] += 1
+                break
+        else:
+            new_item = {
+                "product_id": product_id,
+                "quantity": 1,
+            }
+            self._cart["items"].append(new_item)
+        self.save()
+
+    def clear(self):
+        self._cart = self.session["cart"] = {
+            "items": [],
+        }
+        self.save()
+
+    def get_cart_dict(self):
+        return self._cart
+
+    def get_total_quantity(self):
+        total_quantiy = 0
+        for item in self._cart["items"]:
+            total_quantiy += item["quantity"]
+        return total_quantiy
+
+    def save(self):
+        self.session.modified = True
