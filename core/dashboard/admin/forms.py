7217d9a387456464b5d7316169e23b21f57274a6
Farzin Shams <farzinnater@gmail.com>
2025-01-12 18:43:26 +0330

    Creating Security tab for Customer

--- core/dashboard/admin/forms.py
+++ core/dashboard/admin/forms.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.forms import PasswordChangeForm
-from django.utils.translation import gettext_lazy as _
 
 
 class AdminPasswordChangeForm(PasswordChangeForm):
--- core/dashboard/customer/urls.py
+++ core/dashboard/customer/urls.py
@@ -5,4 +5,6 @@ app_name = "customer"
 
 urlpatterns = [
     path("home/", views.CustomerDashboardHomeView.as_view(), name="home"),
+    path("security-edit/", views.CustomerSecurityEditView.as_view(),
+         name="security-edit"),
 ]
