e3c7deea81b27ae2684650099f5ec2c9a1f591fe
Farzin Shams <farzinnater@gmail.com>
2025-01-12 13:39:59 +0330

    Added Access Permission For Users Role

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from dashboard.permissions import HasAdminAccessPermission
 
 
-class AdminDashboardHomeView(LoginRequiredMixin, TemplateView):
+class AdminDashboardHomeView(LoginRequiredMixin, HasAdminAccessPermission, TemplateView):
 
     template_name = "dashboard/admin/home.html"
--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from dashboard.permissions import HasCustomerAccessPermission
 
 
-class CustomerDashboardHomeView(LoginRequiredMixin, TemplateView):
+class CustomerDashboardHomeView(LoginRequiredMixin, HasCustomerAccessPermission, TemplateView):
 
     template_name = "dashboard/customer/home.html"
