a79b206c11a1ea70086fb6ea53b8165fb58a68c0
Farzin Shams <farzinnater@gmail.com>
2025-01-13 12:29:01 +0330

    Added Avatar Field to profile and set default avatar

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -70,17 +70,26 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        'User', on_delete=models.CASCADE, related_name="user_profile")
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15, validators=[
                                     iranian_phone_number_validator])
+    avatar = models.ImageField(
+        upload_to="profile/", default="profile/default.jpg")
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.email
+
+    def get_fullname(self):
+        if self.first_name or self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return "پروفایل خود را تکمیل کنید"
 
 
 @receiver(post_save, sender=User)
