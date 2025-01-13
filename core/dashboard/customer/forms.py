2479ba464940150f29e68bfcdf404c2616752db6
Farzin Shams <farzinnater@gmail.com>
2025-01-13 10:13:17 +0330

    Handle The Error Messages and Customize fields by form Class

--- core/dashboard/customer/forms.py
+++ core/dashboard/customer/forms.py
@@ -2,4 +2,18 @@ from django.contrib.auth.forms import PasswordChangeForm
 
 
 class CustomerPasswordChangeForm(PasswordChangeForm):
-    pass
+    error_messages = {
+        "password_incorrect": _(
+            "رمز عبور فعلی شما به درستی وارد نشده است. لطفا دوباره وارد کنید."
+        ),
+        "password_mismatch": _("فیلدهای رمز عبور جدید با هم برابر نیستند!"),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['old_password'].widget.attrs['class'] = "form-control"
+        self.fields['new_password1'].widget.attrs['class'] = "form-control"
+        self.fields['new_password2'].widget.attrs['class'] = "form-control"
+        self.fields['old_password'].widget.attrs['placeholder'] = "رمز عبور فعلی را وارد کنید."
+        self.fields['new_password1'].widget.attrs['placeholder'] = "رمز عبور جدید را وارد کنید."
+        self.fields['new_password2'].widget.attrs['placeholder'] = "مجدد رمز عبور جدید را وارد کنید."
