931bcf0abeab90c1f1af087ab04f4235716b0b86
Farzin Shams <farzinnater@gmail.com>
2025-01-19 19:45:27 +0330

    Showing the final price
    Calculating shipping and tax by custom utils

---
+++ core/order/utils.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+
+def calculate_tax(total_price):
+    tax_rate = getattr(settings, 'TAX_RATE', 0)  # Default to 0 if not defined
+    return round((total_price * tax_rate) / 100)
+
+
+def calculate_shipping(total_price):
+    free_shipping_threshold = getattr(settings, 'FREE_SHIPPING_THRESHOLD', 0)
+    shipping_fee = getattr(settings, 'SHIPPING_FEE', 0)
+
+    return 0 if total_price > free_shipping_threshold else shipping_fee
--- core/order/views.py
+++ core/order/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import UserAddress
 from .forms import CheckOutForm
 from cart.models import Cart
+from .utils import calculate_tax, calculate_shipping
 
 
 class OrderCheckOutView(LoginRequiredMixin, FormView):
@@ -12,8 +13,20 @@ class OrderCheckOutView(LoginRequiredMixin, FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        cart = Cart.objects.get(user=self.request.user)
+        try:
+            cart = Cart.objects.get(user=self.request.user)
+        except Cart.DoesNotExist:
+            context['error'] = "کارت پیدا نشد."
+            return context
+
         context['addresses'] = UserAddress.objects.filter(
             user=self.request.user)
-        context['total_price'] = cart.calculate_total_price()
+
+        total_price = cart.calculate_total_price()
+        total_tax = calculate_tax(total_price)
+        shipping_cost = calculate_shipping(total_price)
+
+        context['shipping_cost'] = shipping_cost
+        context['total_tax'] = total_tax
+        context['total_price'] = total_price + total_tax + shipping_cost
         return context
