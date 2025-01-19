29841fbc16e729f29f07b7cbed6d5fedb383b368
Farzin Shams <farzinnater@gmail.com>
2025-01-19 17:25:00 +0330

    Start Configuration of Checkout Page

--- core/order/views.py
+++ core/order/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import UserAddress
 
 
 class OrderCheckOutView(LoginRequiredMixin, TemplateView):
     template_name = "order/checkout.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['addresses'] = UserAddress.objects.filter(
+            user=self.request.user)
+        return context
