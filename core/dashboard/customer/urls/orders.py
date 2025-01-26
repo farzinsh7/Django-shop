31009403d228963b5a55b26820c9056668e27e56
Farzin Shams <farzinnater@gmail.com>
2025-01-26 14:05:50 +0330

    Creating Invoice View

--- core/dashboard/customer/urls/orders.py
+++ core/dashboard/customer/urls/orders.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("orders/list/", views.CustomerOrdersListView.as_view(), name="orders-list"),
     path("orders/<int:pk>/detail/", views.CustomerOrderDetailView.as_view(),
          name="order-detail"),
+    path("orders/<int:pk>/invoice/", views.CustomerOrderInvoiceView.as_view(),
+         name="order-invoice"),
 ]
--- core/dashboard/customer/views/orders.py
+++ core/dashboard/customer/views/orders.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from dashboard.permissions import HasCustomerAccessPermission
-from dashboard.customer.forms import UserAddressForm
-from order.models import UserAddress
 from django.core import exceptions
-from order.models import Order
+from order.models import Order, OrderStatus
+from order.utils import calculate_tax, calculate_shipping
+from decimal import Decimal
 
 
 class CustomerOrdersListView(LoginRequiredMixin, HasCustomerAccessPermission, ListView):
@@ -14,7 +12,7 @@ class CustomerOrdersListView(LoginRequiredMixin, HasCustomerAccessPermission, Li
 
     def get_queryset(self):
         queryset = Order.objects.prefetch_related(
-            'order_items__product').filter(user=self.request.user)
+            'order_items__product').filter(user=self.request.user).order_by('-created_at')
         if search_q := self.request.GET.get("q"):
             queryset = queryset.filter(title__icontains=search_q)
         if order_by := self.request.GET.get("order_by"):
@@ -35,3 +33,36 @@ class CustomerOrderDetailView(LoginRequiredMixin, HasCustomerAccessPermission, D
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
+
+
+class CustomerOrderInvoiceView(LoginRequiredMixin, HasCustomerAccessPermission, DetailView):
+    template_name = "dashboard/customer/orders/order-invoice.html"
+
+    def get_queryset(self):
+        return Order.objects.filter(status=OrderStatus.deliverd.value).filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        order = self.object
+        coupon = order.coupon
+
+        total_price = sum(
+            item.price * item.quantity for item in order.order_items.all())
+
+        # Apply coupon discount if available
+        discount_amount = 0
+        if order.coupon:
+            discount_amount = round(
+                total_price * Decimal(order.coupon.discount_percent) / 100)
+            total_price -= discount_amount
+
+        total_tax = calculate_tax(total_price)
+        shipping_cost = calculate_shipping(total_price)
+
+        context['discount_amount'] = discount_amount
+        context['coupon'] = coupon
+        context['shipping_cost'] = shipping_cost
+        context['total_tax'] = total_tax
+
+        return context
