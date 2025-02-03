2600adfcbb8fbccc090b97de15c82aa1e02c6d67
Farzin Shams <farzinnater@gmail.com>
2025-02-03 15:25:16 +0330

    Start App review

--- core/core/settings.py
+++ core/core/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'cart',
     'order',
     'payment',
+    'review',
 ]
 
 MIDDLEWARE = [
---
+++ core/review/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+app_name = "review"
+
+urlpatterns = [
+    # path("home/", views.DashboardHomeView.as_view(), name="home"),
+]
