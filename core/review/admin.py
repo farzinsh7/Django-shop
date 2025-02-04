64852d70bc4a1b1ca84da9cd6d94f3edd807333f
Farzin Shams <farzinnater@gmail.com>
2025-02-04 11:29:15 +0330

    Creating review modal

--- core/review/admin.py
+++ core/review/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product', 'rate', 'status', 'created_at']
