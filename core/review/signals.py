0d40307d785e917a86e0431d81d11f45fe45faec
Farzin Shams <farzinnater@gmail.com>
2025-02-04 16:14:39 +0330

    Showing average rating in entire site

--- core/review/signals.py
+++ core/review/signals.py
@@ -1,9 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from shop.models import Product
-from .models import Review
+from .models import Review, ReviewStatusType
+from django.db.models import Avg
 
 
 @receiver(post_save, sender=Review)
 def calculate_avg_rate_review(sender, **kwargs):
-    pass
+    if kwargs['instance'].status == ReviewStatusType.accepted.value:
+        product = kwargs['instance'].product
+        average_rating = Review.objects.filter(
+            product=product, status=ReviewStatusType.accepted).aggregate(Avg('rate'))['rate__avg']
+        product.avg_rate = round(average_rating, 1)
+        product.save()
